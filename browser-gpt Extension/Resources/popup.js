6d60a8e5aae5657b2ecda7f0b951337d62e1c555
Jade Allen Cook <jadeallencook@gmail.com>
2023-12-08 23:40:57 -0800

    setup local storage to store api key

--- browser-gpt Extension/Resources/popup.js
+++ browser-gpt Extension/Resources/popup.js
@@ -1,1 +1,8 @@
-console.log("Hello World!", browser);
+const OPEN_AI_API_KEY = localStorage.getItem("OPEN_AI_API_KEY");
+console.log({ OPEN_AI_API_KEY });
+
+document.getElementById("api").onsubmit = function () {
+  console.log("api");
+  const key = document.getElementById("key").value;
+  localStorage.setItem("OPEN_AI_API_KEY", key);
+};
